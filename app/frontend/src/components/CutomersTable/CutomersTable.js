960634928bb83b92614d33c83651bb8fbed88be1
m4rcos-dev <dev.marcospereira@gmail.com>
2023-04-18 16:25:18 -0300

    :sparkles: feat: add data in CustomerData

--- app/frontend/src/components/CutomersTable/CutomersTable.js
+++ app/frontend/src/components/CutomersTable/CutomersTable.js
@@ -6,10 +6,11 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { CustomerData } from '../../data/CustomerData';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
+    backgroundColor: theme.palette.secondary.main,
     color: theme.palette.common.white,
   },
   [`&.${tableCellClasses.body}`]: {
@@ -27,39 +28,25 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   },
 }));
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
 function CutomersTable() {
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+    <TableContainer component={Paper} sx={{ marginLeft: 6, minWidth: 200, maxWidth: 1600 }}>
+      <Table sx={{ minWidth: 200, maxWidth: 1600 }} aria-label="customized table">
         <TableHead>
           <TableRow>
             <StyledTableCell>Cliente</StyledTableCell>
-            <StyledTableCell align="right">Carro</StyledTableCell>
+            <StyledTableCell align="left">Carro</StyledTableCell>
             <StyledTableCell align="right">Qr Code</StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {CustomerData.map((row) => (
             <StyledTableRow key={row.name}>
               <StyledTableCell component="th" scope="row">
                 {row.name}
               </StyledTableCell>
-              <StyledTableCell align="right">{row.calories}</StyledTableCell>
-              <StyledTableCell align="right">{row.fat}</StyledTableCell>
-              <StyledTableCell align="right">{row.carbs}</StyledTableCell>
-              <StyledTableCell align="right">{row.protein}</StyledTableCell>
+              <StyledTableCell align="left">{row.car}</StyledTableCell>
+              <StyledTableCell align="right">Gerar</StyledTableCell>
             </StyledTableRow>
           ))}
         </TableBody>
---
+++ app/frontend/src/data/CustomerData.js
@@ -0,0 +1,43 @@
+export const CustomerData = [
+  {
+    name: 'Rafael Cavalcanti Sousa',
+    car: 'Chevrolet Sonic LT'
+  },
+  {
+    name: 'Carlos Rocha Martins',
+    car: 'Kia Picanto EX'
+  },
+  {
+    name: 'Ágatha Cardoso Cavalcanti',
+    car: 'Fiat Argo FIREFLY DRIVE'
+  },
+  {
+    name: 'Emilly Lima Ribeiro',
+    car: 'Volvo S60 T4 FWD'
+  },
+  {
+    name: 'Douglas Gomes Barbosa',
+    car: 'Rnault Duster DYNAMIQUE'
+  },
+  {
+    name: 'Maria Barros Almeida',
+    car: 'Ford Focus SE PLUS SEDAN'
+  },
+  {
+    name: 'Kauê Carvalho Cunha',
+    car: 'Volkswagen Jetta CONFORTLINE'
+  },
+  {
+    name: 'Victor Carvalho Pinto',
+    car: 'Honda City EX'
+  },
+  {
+    name: 'Kauã Cavalcanti Oliveira',
+    car: 'Fiat 500 CABRIO'
+  },
+  {
+    name: 'Brenda Rodrigues Melo',
+    car: 'Fiat Cronos E.TORQ PRECISION'
+  },
+
+]
