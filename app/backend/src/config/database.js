8df2ed49110b6785bb767e9f5055bf088cd2e804
m4rcos-dev <dev.marcospereira@gmail.com>
2023-04-18 08:51:20 -0300

    :sparkles: feat: add database config

---
+++ app/backend/src/config/database.js
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+
+const config = {
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || '123456',
+  database: 'ULTRACAR_SERVICE',
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT) || 3002,
+  dialect: 'mysql',
+  dialectOptions: {
+    timezone: 'Z',
+  },
+  logging: false,
+}
+
+module.exports = config;
