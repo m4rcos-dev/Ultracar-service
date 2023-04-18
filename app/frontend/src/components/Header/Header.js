b11433f273ed3727047535ee807a1ecc817b6fbe
m4rcos-dev <dev.marcospereira@gmail.com>
2023-04-17 23:30:24 -0300

    :lipstick: feat: create HeaderStyle

--- app/frontend/src/components/Header/Header.js
+++ app/frontend/src/components/Header/Header.js
@@ -1,32 +1,11 @@
-import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import MuiAppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import { useContext } from 'react';
 import { ContextConfig } from '../../context/ContextConfig';
-
-const drawerWidth = 240;
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
+import { AppBar } from './HeaderStyle';
 
 function Header() {
   const { valueContext, handleMenuOpen } = useContext(ContextConfig)
