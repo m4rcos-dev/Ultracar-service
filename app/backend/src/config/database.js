da3d38d616d6330635746868e66162f4c455b1a2
m4rcos-dev <dev.marcospereira@gmail.com>
2023-04-18 09:25:24 -0300

    :wrench: chore: create docker-compose to database and refactor database.js in config

--- app/backend/src/config/database.js
+++ app/backend/src/config/database.js
@@ -1,11 +1,11 @@
-import 'dotenv/config';
+require('dotenv').config();
 
 const config = {
   username: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || '123456',
   database: 'ULTRACAR_SERVICE',
   host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3002,
+  port: Number(process.env.DB_PORT) || 3306,
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
