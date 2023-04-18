7d0d1533c9ade25d74fcd2cd052ff8f61ddbfdf1
m4rcos-dev <dev.marcospereira@gmail.com>
2023-04-17 21:23:07 -0300

    :lipstick: feat: add CssBaseline to reset css

--- app/frontend/src/index.js
+++ app/frontend/src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import theme from './defaultStyles/theme';
-import { ThemeProvider } from '@mui/material';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <App />
     </ThemeProvider>
   </React.StrictMode>
