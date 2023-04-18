5069f26065aea40ab569b01f8ca4e210e0518c42
m4rcos-dev <dev.marcospereira@gmail.com>
2023-04-18 16:29:18 -0300

    :sparkles: feat: create CustomerTableStyle

--- app/frontend/src/components/CutomersTable/CutomersTable.js
+++ app/frontend/src/components/CutomersTable/CutomersTable.js
@@ -1,32 +1,11 @@
-import { styled } from '@mui/material/styles';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { CustomerData } from '../../data/CustomerData';
-
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.secondary.main,
-    color: theme.palette.common.white,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
-}));
-
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
-    backgroundColor: theme.palette.action.hover,
-  },
-  // hide last border
-  '&:last-child td, &:last-child th': {
-    border: 0,
-  },
-}));
+import { StyledTableCell, StyledTableRow } from './CustomerTableStyle';
 
 function CutomersTable() {
   return (
