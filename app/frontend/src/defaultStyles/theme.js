95be00937f31566df3e7fe2b78f7c9e5f99eef66
m4rcos-dev <dev.marcospereira@gmail.com>
2023-04-17 21:19:24 -0300

    :lipstick: feat: add ThemeProvider

---
+++ app/frontend/src/defaultStyles/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    // primary: {
+    //   main: purple[500],
+    // },
+    secondary: {
+      main: "#9a9c9c",
+    },
+  },
+});
+
+export default theme;
