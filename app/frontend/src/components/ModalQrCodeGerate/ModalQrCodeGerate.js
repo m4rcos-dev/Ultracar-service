71fb66874ba75bb8b7eaaf0027f44dde52cf4962
m4rcos-dev <dev.marcospereira@gmail.com>
2023-04-18 20:31:17 -0300

    :sparkles: feat: create ModalQrCodeGerateStyle

--- app/frontend/src/components/ModalQrCodeGerate/ModalQrCodeGerate.js
+++ app/frontend/src/components/ModalQrCodeGerate/ModalQrCodeGerate.js
@@ -6,20 +6,7 @@ import QRCode from 'react-qr-code';
 import QRCodeLink from 'qrcode';
 import { Button, Link } from '@mui/material';
 import DownloadIcon from '@mui/icons-material/Download';
-
-const style = {
-  position: 'absolute',
-  top: '50%',
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
+import { style } from './ModalQrCodeGerateStyle';
 
 function ModalQrCodeGerate() {
   const [qrCodeImg, setQrCodeImg] = useState('');
---
+++ app/frontend/src/components/ModalQrCodeGerate/ModalQrCodeGerateStyle.js
@@ -0,0 +1,13 @@
+export const style = {
+  position: 'absolute',
+  top: '50%',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
