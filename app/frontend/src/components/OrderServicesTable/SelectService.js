d83be3118feb41a5c63073918dd0b8ca9a8064db
m4rcos-dev <dev.marcospereira@gmail.com>
2023-04-19 05:58:56 -0300

    :sparkles: feat: add functions and validations in buttons

--- app/frontend/src/components/OrderServicesTable/SelectService.js
+++ app/frontend/src/components/OrderServicesTable/SelectService.js
@@ -1,5 +1,5 @@
 import { Button, Input, InputAdornment, TextField } from "@mui/material"
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ContextConfig } from "../../context/ContextConfig";
 import { servicesData } from "../../data/ServiceData"
 import { Autocomplete, Box } from "./SelectMechanicStyle";
@@ -9,10 +9,13 @@ import { ContextData } from "../../context/ContextData";
 function SelectService() {
   const { valueContext } = useContext(ContextConfig);
   const openMenu = valueContext.openMenu;
-  const { handleCurrentService} = useContext(ContextData)
+  const { contextData, handleCurrentService } = useContext(ContextData);
+  const currrentMechanic = contextData.currentCustomer.mechanic;
   const [service, setService] = useState('');
   const [cost, setCost] = useState('');
-  console.log(service);
+  const [disabled, setDisabled] = useState(true)
+  const [disabledFinal, setDisabledFinal] = useState(true);
+  const currentService = contextData.currentService
 
   const handelService = (event) => {
     setService(event.target.textContent);
@@ -29,38 +32,58 @@ function SelectService() {
     })
   }
 
+  useEffect(() => {
+    const isDisable = () => {
+      if (
+        currrentMechanic.length > 0
+        && service.length > 0
+        && cost.length > 0) setDisabled(false);
+    }
+    isDisable();
+  }, [currrentMechanic, service, cost]);
+
+  useEffect(() => {
+    const isDisableFinal = () => {
+      if(currentService.length > 1) setDisabledFinal(false);
+    }
+    isDisableFinal();
+  }, [currentService])
+
+
   return (
     <Box>
-        <Autocomplete
-          disablePortal
-          onChange={(e) => handelService(e)}
-          id="combo-box-demo"
-          options={servicesData}
-          sx={{ marginLeft: openMenu ? 30 : 10 }}
-          renderInput={(params) => <TextField {...params} label="Serviço" />}
-        />
-        <Input
-          id="standard-adornment-amount"
-          startAdornment={<InputAdornment position="start">R$</InputAdornment>}
-          value={cost}
-          onChange={handelCost}
-        />
-        <Button
-          variant="contained"
-          endIcon={<AddchartIcon />}
+      <Autocomplete
+        disablePortal
+        onChange={(e) => handelService(e)}
+        id="combo-box-demo"
+        options={servicesData}
+        sx={{ marginLeft: openMenu ? 30 : 10 }}
+        renderInput={(params) => <TextField {...params} label="Serviço" />}
+      />
+      <Input
+        id="standard-adornment-amount"
+        startAdornment={<InputAdornment position="start">R$</InputAdornment>}
+        value={cost}
+        onChange={handelCost}
+      />
+      <Button
+        disabled={disabled}
+        variant="contained"
+        endIcon={<AddchartIcon />}
         // sx={button}
         onClick={addService}
-        >
-          Adicionar Serviço
-        </Button>
-        <Button
-          variant="contained"
-          endIcon={<AddchartIcon />}
-        // sx={button}
-        // onClick={() => handleModalGerate(true)}
-        >
-          Concluir
-        </Button>
+      >
+        Adicionar Serviço
+      </Button>
+      <Button
+        disabled={disabledFinal}
+        variant="contained"
+        endIcon={<AddchartIcon />}
+      // sx={button}
+      // onClick={() => handleModalGerate(true)}
+      >
+        Concluir
+      </Button>
     </Box>
   )
 }
